eef26a820438794309ac28036e67975c7b411930
ImKunYoung <lim8667@gmail.com>
2022-07-13 13:19:11 +0900

    연관관계 테스트

---
+++ src/test/java/com/example/ex5_board/repository/BoardRepositoryTests.java
@@ -0,0 +1,13 @@
+package com.example.ex5_board.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+
+}
---
+++ src/test/java/com/example/ex5_board/repository/MemberRepositoryTests.java
@@ -0,0 +1,14 @@
+package com.example.ex5_board.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemberRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+
+}
