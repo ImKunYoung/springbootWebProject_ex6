54661672a3e98cd9c45bd97efdc0c25f9eece312
ImKunYoung <lim8667@gmail.com>
2022-07-13 12:19:55 +0900

    initial commit

---
+++ src/main/java/com/example/ex5_board/Ex5BoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex5_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex5BoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex5BoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ex5_board/Ex5BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex5_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex5BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
