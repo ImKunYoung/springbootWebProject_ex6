0cb2264720c8fde01fa398fe8aa21d0013981492
ImKunYoung <lim8667@gmail.com>
2022-07-13 14:02:04 +0900

    BoardRepository 테스트 - sample data insert

--- src/test/java/com/example/ex5_board/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/ex5_board/repository/BoardRepositoryTests.java
@@ -1,7 +1,12 @@
 package com.example.ex5_board.repository;
 
+import com.example.ex5_board.entity.Board;
+import com.example.ex5_board.entity.Member;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 public class BoardRepositoryTests {
@@ -9,5 +14,23 @@ public class BoardRepositoryTests {
     @Autowired
     private BoardRepository boardRepository;
 
+    /*INSERT SAMPLE DATA*/
+    @Test
+    public void insertBoard() {
 
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            
+            Member member = Member.builder().email("user"+i+"@aaa.com").build();
+
+            Board board = Board.builder()
+                    .title("Title..."+i)
+                    .content("Content...."+i)
+                    .writer(member)
+                    .build();
+
+            boardRepository.save(board);
+
+        });
+
+    }
 }
