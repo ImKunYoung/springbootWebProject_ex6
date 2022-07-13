518629652295775d2d422660c7fc9e4218f0ce77
ImKunYoung <lim8667@gmail.com>
2022-07-13 13:45:09 +0900

    테스트 안 될때: File | Settings | Build, Execution, Deployment | Build Tools | Gradle | Run Test 뭐시기 -> (Gradle을) Intellj 로 변경

--- src/test/java/com/example/ex5_board/repository/MemberRepositoryTests.java
+++ src/test/java/com/example/ex5_board/repository/MemberRepositoryTests.java
@@ -1,8 +1,11 @@
 package com.example.ex5_board.repository;
 
+import com.example.ex5_board.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 public class MemberRepositoryTests {
@@ -10,5 +13,20 @@ public class MemberRepositoryTests {
     @Autowired
     private MemberRepository memberRepository;
 
+    /*INSERT SAMPLE DATA*/
+    @Test
+    public void insertMembers() {
+
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Member member = Member.builder()
+                    .email("user"+i+"@aaa.com")
+                    .password("1111")
+                    .name("USER"+i)
+                    .build();
+
+            memberRepository.save(member);
+        });
+
+    }
 
 }
