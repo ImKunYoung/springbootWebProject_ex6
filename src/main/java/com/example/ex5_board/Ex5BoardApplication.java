a922e1dbb31fc40d044ee24635538ebac53567b7
ImKunYoung <lim8667@gmail.com>
2022-07-13 12:39:23 +0900

    날짜/시간 처리 설정 - AuditingEntityListener 활성화

--- src/main/java/com/example/ex5_board/Ex5BoardApplication.java
+++ src/main/java/com/example/ex5_board/Ex5BoardApplication.java
@@ -2,8 +2,10 @@ package com.example.ex5_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing /*AuditingEntityListener 활성화를 위함*/
 public class Ex5BoardApplication {
 
     public static void main(String[] args) {
