bc62bb72ac2273b4546bfbe1cb8ddced834801c5
ImKunYoung <lim8667@gmail.com>
2022-07-14 12:35:41 +0900

    엔티티 클래스 내부에 연관관계가 있는 경우 (조인 처리) - repository 쿼리 추가 - Left join (on 제외)
    지연 로딩
    - 장점: 조인 하지 않을 때 조회가 빠름
    - 단점: 연관관계가 복잡한 경우 여러 번의 쿼리가 실행됨

--- src/main/java/com/example/ex5_board/repository/BoardRepository.java
+++ src/main/java/com/example/ex5_board/repository/BoardRepository.java
@@ -1,7 +1,14 @@
 package com.example.ex5_board.repository;
 
-import com.example.ex5_board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.example.ex5_board.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    /*한개의 로우(Object) 내에 Object[]로 나옴*/
+    @Query("select b, w from Board b left join b.writer w where b.bno =:bno")
+    Object getBoardWithWriter(@Param("bno") Long bno);
+
 }
