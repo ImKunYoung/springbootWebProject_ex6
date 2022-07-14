f6c824480dc00f4c50ba1917703bbca671be2d21
ImKunYoung <lim8667@gmail.com>
2022-07-14 13:01:32 +0900

    연관관계가 없는 엔티티 조인 처리에는 on

--- src/main/java/com/example/ex5_board/repository/BoardRepository.java
+++ src/main/java/com/example/ex5_board/repository/BoardRepository.java
@@ -5,11 +5,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import com.example.ex5_board.entity.Board;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    /*한개의 로우(Object) 내에 Object[]로 나옴*/
-    String value = "select b, w from Board b left join b.writer w where b.bno =:bno";
-    @Query(value)
+    /*연관관계가 있는 엔티티 조인*/
+    String q1 = "select b, w from Board b left join b.writer w where b.bno =:bno";
+    @Query(q1)
     Object getBoardWithWriter(@Param("bno") Long bno);
+
+    /*연관관계가 없는 엔티티 조인*/
+    String q2 = "SELECT b, r, FROM Board b LEFT JOIN Reply r ON r.board = b WHERE b.bno = :bno";
+    @Query(q2)
+    List<Object[]> getBoardWithReply(@Param("bno") Long bno);
 
 }
