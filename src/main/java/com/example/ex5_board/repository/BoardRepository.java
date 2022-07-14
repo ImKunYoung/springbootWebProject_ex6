89860572d71f81d798e395c08e49179476b986c4
ImKunYoung <lim8667@gmail.com>
2022-07-14 13:39:12 +0900

    목록화면 query

--- src/main/java/com/example/ex5_board/repository/BoardRepository.java
+++ src/main/java/com/example/ex5_board/repository/BoardRepository.java
@@ -1,5 +1,7 @@
 package com.example.ex5_board.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,12 +13,18 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     /*연관관계가 있는 엔티티 조인*/
     String q1 = "select b, w from Board b left join b.writer w where b.bno =:bno";
-    @Query(q1)
+    @Query(value = q1)
     Object getBoardWithWriter(@Param("bno") Long bno);
 
     /*연관관계가 없는 엔티티 조인 +ON*/
     String q2 = "SELECT b, r FROM Board b LEFT JOIN Reply r ON r.board = b WHERE b.bno = :bno";
-    @Query(q2)
+    @Query(value = q2)
     List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    /*목록 화면*/
+    String q3 = "SELECT b, w, count(r) FROM Board b LEFT JOIN b.writer w LEFT JOIN Reply r ON r.board = b GROUP BY b";
+    String q4 = "SELECT count(b) FROM Board b";
+    @Query(value = q3, countQuery = q4)
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable);
 
 }
--- src/test/java/com/example/ex5_board/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/ex5_board/repository/BoardRepositoryTests.java
@@ -5,6 +5,10 @@ import com.example.ex5_board.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
@@ -79,4 +83,17 @@ public class BoardRepositoryTests {
         }
 
     }
+
+    /*목록 화면*/
+    @Test
+    public void testWithReplyCount() {
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row -> System.out.println(Arrays.toString(row)));
+
+    }
+
 }
