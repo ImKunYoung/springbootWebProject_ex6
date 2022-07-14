9aa7f6e04de1ce28086e38c95d5608fc54a0916a
ImKunYoung <lim8667@gmail.com>
2022-07-14 11:18:44 +0900

    테이블 조인
    Hibernate:
        select
            reply0_.rno as rno1_2_0_,
            reply0_.moddate as moddate2_2_0_,
            reply0_.regdate as regdate3_2_0_,
            reply0_.board_bno as board_bn6_2_0_,
            reply0_.replyer as replyer4_2_0_,
            reply0_.text as text5_2_0_,
            board1_.bno as bno1_0_1_,
            board1_.moddate as moddate2_0_1_,
            board1_.regdate as regdate3_0_1_,
            board1_.content as content4_0_1_,
            board1_.title as title5_0_1_,
            board1_.writer_email as writer_e6_0_1_,
            member2_.email as email1_1_2_,
            member2_.moddate as moddate2_1_2_,
            member2_.regdate as regdate3_1_2_,
            member2_.name as name4_1_2_,
            member2_.password as password5_1_2_
        from
            reply reply0_
        left outer join
            board board1_
                on reply0_.board_bno=board1_.bno
        left outer join
            member member2_
                on board1_.writer_email=member2_.email
        where
            reply0_.rno=?
    Reply(rno=1, text=Reply.......1, replyer=guest, board=Board(bno=115, title=Title...14, content=Content....14, writer=Member(email=user14@aaa.com, password=1111, name=USER14)))
    Board(bno=115, title=Title...14, content=Content....14, writer=Member(email=user14@aaa.com, password=1111, name=USER14))

--- src/test/java/com/example/ex5_board/repository/ReplyRepositoryTests.java
+++ src/test/java/com/example/ex5_board/repository/ReplyRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -34,6 +35,26 @@ public class ReplyRepositoryTests {
             replyRepository.save(reply);
 
         });
+    }
+
+    /*테이블 조인*/
+    /*SELECT * FROM reply
+    LEFT OUTER JOIN board ON reply.board_bno = board.bno
+    LEFT OUTER JOIN member ON member.email = board.writer_email
+    WHERE reply.rno = '1';*/
+    @Test
+    public void readReply1() {
+
+        Optional<Reply> result = replyRepository.findById(1L);
+
+        if(result.isPresent()) {
+            Reply reply = result.get();
+
+            System.out.println(reply);
+            System.out.println(reply.getBoard());
+
+        }
+
     }
 
 }
