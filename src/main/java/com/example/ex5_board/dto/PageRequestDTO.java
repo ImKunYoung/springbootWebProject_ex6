7d32c61abcd4e17dad25f4436672492c82332815
ImKunYoung <lim8667@gmail.com>
2022-07-15 12:12:57 +0900

    게시물 목록 처리

---
+++ src/main/java/com/example/ex5_board/dto/PageRequestDTO.java
@@ -0,0 +1,34 @@
+package com.example.ex5_board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class PageRequestDTO {
+
+    private int page;
+    private int size;
+
+    /*검색 처리 1- 서버측 검색 처리*/
+    /*검색 조건*/
+    private String type;
+    /*검색 키워드*/
+    private String keyword;
+
+
+    public PageRequestDTO() {
+        this.page = 1;
+        this.size = 10;
+    }
+
+    public Pageable getPageable(Sort sort) {
+        return PageRequest.of(page-1, size, sort);
+    }
+
+}
