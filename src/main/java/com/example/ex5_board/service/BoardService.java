cf0dcf41f11d814a0d18bd7ab736c6ef936f9d46
ImKunYoung <lim8667@gmail.com>
2022-07-15 11:33:38 +0900

    게시물 등록 - BoardService

---
+++ src/main/java/com/example/ex5_board/service/BoardService.java
@@ -0,0 +1,24 @@
+package com.example.ex5_board.service;
+
+import com.example.ex5_board.BoardDTO.BoardDTO;
+import com.example.ex5_board.entity.Board;
+import com.example.ex5_board.entity.Member;
+
+public interface BoardService {
+    /*게시물 등록*/
+    Long register(BoardDTO dto);
+
+    default Board dtoToEntity(BoardDTO dto) {
+
+        Member member = Member.builder().email(dto.getWriterEmail()).build();
+
+        return Board.builder()
+                .bno(dto.getBno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(member)
+                .build();
+
+    }
+
+}
