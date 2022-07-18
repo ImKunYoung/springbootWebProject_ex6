466d7396213474d096579cf0d4b534ec5fcff124
ImKunYoung <lim8667@gmail.com>
2022-07-18 10:07:14 +0900

    board 화면 처리 - 컨트롤러

--- src/main/java/com/example/ex5_board/controller/BoardController.java
+++ src/main/java/com/example/ex5_board/controller/BoardController.java
@@ -1,9 +1,12 @@
 package com.example.ex5_board.controller;
 
+import com.example.ex5_board.dto.PageRequestDTO;
 import com.example.ex5_board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,4 +17,11 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    /*목록 스크린 처리*/
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+        model.addAttribute("result", boardService.getList(pageRequestDTO));
+
+    }
 }
