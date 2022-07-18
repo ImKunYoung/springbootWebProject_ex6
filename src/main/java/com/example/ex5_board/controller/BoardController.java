61c5c50d69fb89d1b9fa449d958f3649d1ce07de
ImKunYoung <lim8667@gmail.com>
2022-07-18 16:33:10 +0900

    board 화면 처리 - 게시물 조회 하기(/read) - 컨트롤러

--- src/main/java/com/example/ex5_board/controller/BoardController.java
+++ src/main/java/com/example/ex5_board/controller/BoardController.java
@@ -5,9 +5,11 @@ import com.example.ex5_board.dto.PageRequestDTO;
 import com.example.ex5_board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -57,6 +59,22 @@ public class BoardController {
         return "redirect:/board/list";
 
     }
+
+
+    /*게시물 조회하기*/
+    @GetMapping("/read")
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model) {
+
+        log.info("bno: " + bno);
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
+
+    }
+
 
 
 }
