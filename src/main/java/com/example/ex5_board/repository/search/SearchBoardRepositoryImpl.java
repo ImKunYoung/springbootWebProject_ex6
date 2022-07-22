46c4485bc03bcaba094821965e5f6e34920071b7
ImKunYoung <lim8667@gmail.com>
2022-07-22 18:43:14 +0900

    JPQLQuery 조인
    Hibernate:
        select
            board0_.bno as col_0_0_,
            member1_.email as col_1_0_,
            count(reply2_.rno) as col_2_0_,
            board0_.bno as bno1_0_,
            board0_.moddate as moddate2_0_,
            board0_.regdate as regdate3_0_,
            board0_.content as content4_0_,
            board0_.title as title5_0_,
            board0_.writer_email as writer_e6_0_
        from
            board board0_
        left outer join
            member member1_
                on (
                    board0_.writer_email=member1_.email
                )
        left outer join
            reply reply2_
                on (
                    reply2_.board_bno=board0_.bno
                )
        group by
            board0_.bno

--- src/main/java/com/example/ex5_board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/com/example/ex5_board/repository/search/SearchBoardRepositoryImpl.java
@@ -2,6 +2,8 @@ package com.example.ex5_board.repository.search;
 
 import com.example.ex5_board.entity.Board;
 import com.example.ex5_board.entity.QBoard;
+import com.example.ex5_board.entity.QMember;
+import com.example.ex5_board.entity.QReply;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
@@ -23,10 +25,14 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
         log.info("search1............................");
 
         QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
 
         JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
 
-        jpqlQuery.select(board).where(board.bno.eq(2L));
+        jpqlQuery.select(board, member.email, reply.count()).groupBy(board);
 
         log.info("_________________________________________________");
         log.info(jpqlQuery);
