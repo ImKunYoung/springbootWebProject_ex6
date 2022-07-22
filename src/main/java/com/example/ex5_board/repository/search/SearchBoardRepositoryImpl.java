2aa817de6c52e560bbc50da1f292c98e25dcd3c6
ImKunYoung <lim8667@gmail.com>
2022-07-22 22:21:25 +0900

    JPQLQuery로 sort 처리 / count

--- src/main/java/com/example/ex5_board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/com/example/ex5_board/repository/search/SearchBoardRepositoryImpl.java
@@ -6,14 +6,20 @@ import com.example.ex5_board.entity.QMember;
 import com.example.ex5_board.entity.QReply;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Log4j2
 public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
@@ -87,13 +93,37 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
 
         tuple.where(booleanBuilder);
 
+        /*Order By*/
+        Sort sort = pageable.getSort();
+
+        // tuple.orderBy(board.bno.desc());
+
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+            String prop = order.getProperty();
+
+            PathBuilder orderByExpression = new PathBuilder(Board.class, "board");
+
+            tuple.orderBy(new OrderSpecifier(direction, orderByExpression.get(prop)));
+
+        });
+
         tuple.groupBy(board);
+
+
+        /*Page 처리*/
+        tuple.offset(pageable.getOffset());
+        tuple.limit(pageable.getPageSize());
 
         List<Tuple> result = tuple.fetch();
 
         log.info(result);
 
-        return null;
+        long count = tuple.fetchCount();
+
+        log.info("COUNT: "+ count);
+
+        return new PageImpl<Object[]>(result.stream().map(t -> t.toArray()).collect(Collectors.toList()), pageable, count);
 
     }
 
