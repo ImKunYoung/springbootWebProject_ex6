f5ff535fe378a1b3adb8fbb5bd88b290a44358a4
ImKunYoung <lim8667@gmail.com>
2022-07-22 18:54:48 +0900

    JPQLQuery로 Page<Object[]> 처리 - interface

--- src/main/java/com/example/ex5_board/repository/search/SearchBoardRepository.java
+++ src/main/java/com/example/ex5_board/repository/search/SearchBoardRepository.java
@@ -1,9 +1,13 @@
 package com.example.ex5_board.repository.search;
 
 import com.example.ex5_board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface SearchBoardRepository {
 
     Board search1();
+
+    Page<Object[]> searchPage(String type, String keyword, Pageable pageable);
 
 }
