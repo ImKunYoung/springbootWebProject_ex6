a92a0358bcf16f80f3bdd320bd0a7dddc5e081f0
ImKunYoung <lim8667@gmail.com>
2022-07-22 18:30:17 +0900

    repository 확장 : JPQLQuery 객체 - 쿼리 메서드나 @Query 등으로 처리할 수 없는 기능ㅇ느 별도의 인터페이스로 설계 - 별도 인터페이스 형상화 클래스 작성. 이때 QuerydRepositorySupport(from Spring Data JPA Lib)라는 클래스를 부모 클래스로 사용 - 구현 클래스에 인터페이스의 기능을 Q도메인 클래스와 JPQLQuery를 이용해서 구현

--- src/main/java/com/example/ex5_board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/com/example/ex5_board/repository/search/SearchBoardRepositoryImpl.java
@@ -1,8 +1,12 @@
 package com.example.ex5_board.repository.search;
 
 import com.example.ex5_board.entity.Board;
+import com.example.ex5_board.entity.QBoard;
+import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
 
 @Log4j2
 public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
@@ -12,12 +16,26 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
         super(Board.class);
     }
 
+    /*JPQL Query 객체*/
     @Override
     public Board search1() {
 
         log.info("search1............................");
 
-        return null;
+        QBoard board = QBoard.board;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+
+        jpqlQuery.select(board).where(board.bno.eq(2L));
+
+        log.info("_________________________________________________");
+        log.info(jpqlQuery);
+        log.info("_________________________________________________");
+
+
+        List<Board> result = jpqlQuery.fetch();
+
+        return result.get(0);
 
     }
 
