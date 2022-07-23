04a30dcadac2516b04dea2e5f9823a9b362653fe
ImKunYoung <lim8667@gmail.com>
2022-07-23 17:05:11 +0900

    JSON 과 Ajax 로 댓글 처리 - service 인터페이스 정의
    1. 댓글 등록하는 기능 register
    2. 특정 게시물 댓글 리스트 가져오는 기능 getList
    3. 댓글을 수정하는 기능 modify
    4. 댓글 삭제하는 기능 remove
    5. 엔티티 -> dto
    6. dto -> entity
    
    요구사항
    // 게시물의 댓글을 확인/추가/수정/삭제 할 수 있다.
    A1. read 화면이 로딩된다
    A2. read 화면에서 댓글의 숫자를 클릭한다. (확인은 A3, 추가는 B2, 수정/삭제는 C3을 따른다)
    A3. 해당 게시물에 속한 댓글을 Ajax로 가져온다.
    A4. Ajax로 가져온 댓글을 화면에 보여준다.
    
    B3. 특정한 버튼을 클릭한다.
    B4. 새로운 댓글을 입력할 수 있는 모달창을 보여준다.
    B5. Ajax의 Post 방식으로 댓글을 전송한다.
    B6. 댓글의 목록을 새로 가져오고 A1을 따른다.
    
    C3.  특정한 댓글을 선택한다.
    C4. 댓글을 삭제/수정....모달창

--- src/main/java/com/example/ex5_board/service/ReplyService.java
+++ src/main/java/com/example/ex5_board/service/ReplyService.java
@@ -1,17 +1,56 @@
 package com.example.ex5_board.service;
 
+import com.example.ex5_board.dto.ReplyDTO;
+import com.example.ex5_board.entity.Board;
+import com.example.ex5_board.entity.Reply;
+
+import java.util.List;
+
 public interface ReplyService {
 
     /*댓글을 등록한다*/
+    Long register(ReplyDTO replyDTO);
 
     /*특정 게시물의 댓글 리스트를 가지고 온다*/
+    List<ReplyDTO> getList(Long bno);
 
     /*댓글을 수정한다*/
+    void modify(ReplyDTO replyDTO);
 
     /*댓글을 삭제한다*/
+    void remove(Long rno);
 
     /*ReplyEntity -> ReplyDTO*/
+    default Reply dtoToEntity(ReplyDTO replyDTO) {
+
+        Board board = Board.builder().bno(replyDTO.getBno()).build();
+
+        Reply reply = Reply.builder()
+                .rno(replyDTO.getRno())
+                .text(replyDTO.getText())
+                .replyer(replyDTO.getReplyer())
+                .board(board)
+                .build();
+
+        return reply;
+
+    }
 
     /*ReplyDTO -> ReplyEntity*/
+    default ReplyDTO dtoToEntity(Reply reply) {
+
+        // Reply 객체를 ReplyDTO 로 변환 시 Board 객체가 필요하지 않으므로 게시물 번호만
+        ReplyDTO dto = ReplyDTO.builder()
+                .rno(reply.getRno())
+                .text(reply.getText())
+                .replyer(reply.getReplyer())
+                .regDate(reply.getRegDate())
+                .modDate(reply.getModDate())
+                .build();
+
+
+        return dto;
+
+    }
 
 }
