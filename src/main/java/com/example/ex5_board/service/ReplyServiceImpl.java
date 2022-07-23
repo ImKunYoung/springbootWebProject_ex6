0757d38924cc068f6b2686f38663226b8b957125
ImKunYoung <lim8667@gmail.com>
2022-07-23 17:13:50 +0900

    JSON 과 Ajax 로 댓글 처리 - service 구현체 - 댓글 등록, 특정 게시물 댓글 리스트 가져오기
    1. 댓글 등록하는 기능 register
    2. 특정 게시물 댓글 리스트 가져오는 기능 getList
    3. 댓글을 수정하는 기능 modify
    4. 댓글 삭제하는 기능 remove
    5. 엔티티 -> dto
    6. dto -> entity

--- src/main/java/com/example/ex5_board/service/ReplyServiceImpl.java
+++ src/main/java/com/example/ex5_board/service/ReplyServiceImpl.java
@@ -1,6 +1,56 @@
 package com.example.ex5_board.service;
 
+import com.example.ex5_board.dto.ReplyDTO;
+import com.example.ex5_board.entity.Board;
+import com.example.ex5_board.entity.Reply;
+import com.example.ex5_board.repository.ReplyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
 public class ReplyServiceImpl implements ReplyService {
 
+    private final ReplyRepository replyRepository;
 
+    @Override
+    public Long register(ReplyDTO replyDTO) {
+
+        log.info(replyDTO);
+
+        Reply reply = dtoToEntity(replyDTO);
+
+        replyRepository.save(reply);
+
+        return reply.getRno();
+
+    }
+
+    @Override
+    public List<ReplyDTO> getList(Long bno) {
+
+        log.info(bno);
+
+        Board board = Board.builder().bno(bno).build();
+
+        List<Reply> result = replyRepository.getRepliesByBoardOrderByRno(board);
+
+        return result.stream().map(this::entityToDTO).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public void modify(ReplyDTO replyDTO) {
+
+    }
+
+    @Override
+    public void remove(Long rno) {
+
+    }
 }
