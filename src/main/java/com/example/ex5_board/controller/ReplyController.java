b488a7bf360f8665af03ba21c5eee9682aaf185f
ImKunYoung <lim8667@gmail.com>
2022-07-23 17:33:30 +0900

    JSON 과 Ajax 로 댓글 처리 - 컨트롤러 세팅

---
+++ src/main/java/com/example/ex5_board/controller/ReplyController.java
@@ -0,0 +1,13 @@
+package com.example.ex5_board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/replies/")
+@Log4j2
+@RequiredArgsConstructor
+public class ReplyController {
+}
