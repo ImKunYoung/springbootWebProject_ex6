7b5f0448729b60a02686d82cfd7a6fa9547aee86
ImKunYoung <lim8667@gmail.com>
2022-07-23 16:53:53 +0900

    JSON 과 Ajax 로 댓글 처리 - ReplyDTO
    요구사항
    // 게시물의 댓글을 확인/추가/수정/삭제 할 수 있다.
    A1. read 화면이 로딩된다
    A2. read 화면에서 댓글의 숫자를 클릭한다. (확인은 A3, 추가는 B2, 수정/삭제는 C3을 따른다)
    A3. 해당 게시물에 속한 댓글을 Ajax로 가져온다.
    A4. Ajax로 가져온 댓글을 화면에 보여준다.
    
    B3. 특정한 버튼을 클릭한다.
    B4. 새로운 댓글을 입력할 수 있는 모달창을 보여준다.
    B5. Ajax의 Post 방식으로 댓글을 전송한다.
    B6. 댓글의 목록을 새로 가져오고 A1을 따른다.
    
    C3.  특정한 댓글을 선택한다.
    C4. 댓글을 삭제/수정....모달창

---
+++ src/main/java/com/example/ex5_board/dto/ReplyDTO.java
@@ -0,0 +1,27 @@
+package com.example.ex5_board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ReplyDTO {
+
+    private Long rno;
+
+    private String text;
+
+    private String replyer;
+
+    /*게시물 번호*/
+    private Long bno;
+
+    private LocalDateTime regDate, modDate;
+
+}
