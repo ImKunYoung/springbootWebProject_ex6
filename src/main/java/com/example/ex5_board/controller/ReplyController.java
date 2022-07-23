c538a8ccc259e10d5e8ffed5ff2fff01e0fbb95e
ImKunYoung <lim8667@gmail.com>
2022-07-23 17:39:47 +0900

    JSON 과 Ajax 로 댓글 처리 - Rest 컨트롤러 - 게시물 목록 불러오기

--- src/main/java/com/example/ex5_board/controller/ReplyController.java
+++ src/main/java/com/example/ex5_board/controller/ReplyController.java
@@ -1,13 +1,37 @@
 package com.example.ex5_board.controller;
 
+import com.example.ex5_board.dto.ReplyDTO;
+import com.example.ex5_board.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/replies/")
 @Log4j2
 @RequiredArgsConstructor
 public class ReplyController {
+
+    /*자동주입을 위한 final*/
+    private final ReplyService replyService;
+
+
+    /*게시물 목록 불러오기*/
+    @GetMapping(value = "/board/{bno}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ReplyDTO>> getListByBoard(@PathVariable("bno") Long bno) {
+
+        log.info("bno: "+bno);
+
+        return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+
+    }
+
 }
